14187bbc0d263ed2814a971d88f358bc02014e28
abdullah <albinaliama@gmail.com>
2025-05-27 16:59:49 +0300

    work in progress

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from unittest import case
+import re
+
 
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
@@ -36,14 +38,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(TextNode(item, text_type))
     return new_nodes
 
+def extract_markdown_images(text):
+    return  re.findall(r"!\[(.*?)\]\((.*?)\)",text)
 
-
-# node = TextNode("This is text with a `code block` word", TextType.TEXT)
-# [
-#     TextNode("This is text with a ", TextType.TEXT),
-#     TextNode("code block", TextType.CODE),
-#     TextNode(" word", TextType.TEXT),
-# ]
+def extract_markdown_links(text):
+    return re.findall(r"[^!]\[(.*?)\]\((.*?)\)", text)
 
 
 
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import text_node_to_html_node, split_nodes_delimiter
+from main import text_node_to_html_node, split_nodes_delimiter,extract_markdown_images,extract_markdown_links
 from textnode import TextNode,TextType
 
 class Testmain(unittest.TestCase):
@@ -39,3 +39,14 @@ class Testmain(unittest.TestCase):
 
 
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
