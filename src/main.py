e89da54ebd9e8f8411b7a917705ffceafbd0e63d
Abdullah-Albinali <albinali.abdullah@gmail.com>
2025-05-28 14:39:48 +0300

    work in progress

--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
 import re
 
 
-from src.textnode import TextNode, TextType
-from src.htmlnode import LeafNode
+from textnode import TextNode, TextType
+from htmlnode import LeafNode
 
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
-        case(TextType.NORMAL_TEXT):
+        case TextType.NORMAL_TEXT :
             return LeafNode(tag= None, value = text_node.text, props=None)
-        case(TextType.BOLD_TEXT):
+        case TextType.BOLD_TEXT :
             return LeafNode(tag= "b", value = text_node.text, props=None)
-        case(TextType.ITALIC_TEXT):
+        case TextType.ITALIC_TEXT:
             return LeafNode(tag= "i", value = text_node.text, props=None)
-        case(TextType.CODE_TEXT):
+        case TextType.CODE_TEXT :
             return LeafNode(tag= "code", value = text_node.text, props=None)
-        case (TextType.LINK):
+        case TextType.LINK:
             return LeafNode(tag= "a", value = text_node.text, props={"href": text_node.url})
-        case (TextType.IMAGE):
+        case TextType.IMAGE:
             return LeafNode(tag= "img", value = "", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Invalid text node type")
@@ -46,37 +46,80 @@ def extract_markdown_links(text):
 
 
 def split_nodes_image(old_nodes):
-    # if old_nodes.text == "" or old_nodes.text is None:
-    #     return []
-    #
-    # new_nodes = []
-    # temp =
-    #
-    # return new_nodes
-    pass
+    if old_nodes == [] or old_nodes is None:
+        return []
+
+    new_nodes = []
+
+    for node in old_nodes:
+        temp = extract_markdown_images(node.text)
+
+        word_to_split = node.text
+        for item in temp:
+            split_result = word_to_split.split(f"![{item[0]}]({item[1]})", 1)
+            new_node = TextNode(split_result[0], TextType.NORMAL_TEXT)
+            new_nodes.append(new_node)
+            del split_result[0]
+            new_link_node = TextNode(item[0], TextType.IMAGE, item[1])
+            new_nodes.append(new_link_node)
+            word_to_split = "".join(split_result)
+
+    return new_nodes
 
 
 
 def split_nodes_link(old_nodes):
-    if old_nodes.text == "" or old_nodes.text is None:
+    if old_nodes == [] or old_nodes is None:
         return []
 
     new_nodes = []
-    temp = extract_markdown_links(old_nodes.text)
-    temp2 = []
-    if temp == []:
-        return old_nodes
-    word_to_split = old_nodes.text
-    for item in temp:
-        #TODO fix this
-        temp2.append(word_to_split.split(f"[{item[0]}]({item[1]})", 1)[0])
-        word_to_split = str(word_to_split.split(f"[{item[0]}]({item[1]})", 1)[1:])
 
-    return temp2
+    for node in old_nodes:
+        temp = extract_markdown_links(node.text)
+
+        word_to_split = node.text
+        for item in temp:
+
+            split_result = word_to_split.split(f"[{item[0]}]({item[1]})", 1)
+            new_node = TextNode(split_result[0], TextType.NORMAL_TEXT)
+            new_nodes.append(new_node)
+            del split_result[0]
+            new_link_node = TextNode(item[0], TextType.LINK,item[1])
+            new_nodes.append(new_link_node)
+            word_to_split = "".join(split_result)
+
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    final_result = []
+    new_node = TextNode(text, TextType.NORMAL_TEXT)
+    extract_bold = split_nodes_delimiter(new_node,"**",TextType.BOLD_TEXT)
+    final_result.append(extract_bold[0])
+    final_result.append(extract_bold[1])
+    del extract_bold[0]
+    del extract_bold[0]
+    # print(extract_bold)
+    extract_italic = split_nodes_delimiter(extract_bold[0], "_", TextType.ITALIC_TEXT)
+    final_result.append(extract_italic[0])
+    final_result.append(extract_italic[1])
+    del extract_italic[0]
+    del extract_italic[0]
+    extract_code = split_nodes_delimiter(extract_italic[0], "`", TextType.CODE_TEXT)
+    final_result.append(extract_code[0])
+    final_result.append(extract_code[1])
+    del extract_code[0]
+    del extract_code[0]
+    print(extract_code[0])
+    #ISSUE split_nodes_image is not returning the remaining text
+    extract_image = split_nodes_image([extract_code[0]])
+    print(extract_image)
 
 def main():
     a = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(a)
+    text_to_textnodes("")
 
 
 if __name__ == "__main__":
